560e9ae17c880fdbf1cc590f609479e0506a3d1d
Wisdom Egba <99222194+wiz-js@users.noreply.github.com>
2023-06-06 02:03:33 +0100

    updated

--- src/pages/Bookmark/Bookmark.jsx
+++ src/pages/Bookmark/Bookmark.jsx
@@ -1,9 +1,5 @@
 import React from "react"
 
 export const Bookmark = () => {
-  return (
-    <div className="bg-gradient-to-r from-[#222222] via-[#222222]/80 to-gray-300 opacity-100 h-[100vh]">
-      Bookmark
-    </div>
-  )
+  return <div className="">Bookmark</div>
 }
